ee9e45cd1110c0345f1536d89ab4b7b82bc2eed8
codewitharyanraj <aryanrajlenovoi3@gmail.com>
2026-06-16 05:55:39 +0530

    q3 added with bruteforce

--- code01.cpp
+++ code01.cpp
@@ -3273,7 +3273,7 @@ using namespace std;
 //     for(int j=0;j<n;j++) {
 //         if (i==0 || i==n-1 || j==0 || j==n-1) {
 //         cout << "*"; }
-//         else {cout << " ";}
+//         else {cout << " ";   }
 //     }
 //     cout << endl;   }}
 
